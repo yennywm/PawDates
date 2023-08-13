b4d9bf4f209cbc03776e8947f25ac9b62df40594
yennywm <133797785+yennywm@users.noreply.github.com>
2023-08-13 19:01:33 +0100

    Reordering files

--- PawDates/Gallery Page/gallery.js
+++
@@ -1,8 +0,0 @@
-//Notification popup
-function notification() {
-    alert ('You have no new notifications')
-    console.log('Clicked the button')
-}
-
-let bell = document.getElementById("bell")
-bell.addEventListener('click', notification)
